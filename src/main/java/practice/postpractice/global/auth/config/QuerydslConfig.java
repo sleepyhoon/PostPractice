8937c018e74c5f98174121e87f577fbccda4f16c
sleepyhoon <pokjm99@naver.com>
2024-08-23 11:39:49 +0900

    feat : QueryDsl 세팅

---
+++ src/main/java/practice/postpractice/global/auth/config/QuerydslConfig.java
@@ -0,0 +1,40 @@
+package practice.postpractice.global.auth.config;
+
+import com.querydsl.jpa.JPQLTemplates;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <br>package name   : practice.postpractice.global.auth.config
+ * <br>file name      : QuerydslConfig
+ * <br>date           : 2024-08-23
+ * <pre>
+ * <span style="color: white;">[description]</span>
+ *
+ * </pre>
+ * <pre>
+ * <span style="color: white;">usage:</span>
+ * {@code
+ *
+ * } </pre>
+ * <pre>
+ * modified log :
+ * =======================================================
+ * DATE           AUTHOR               NOTE
+ * -------------------------------------------------------
+ * 2024-08-23        SeungHoon              init create
+ * </pre>
+ */
+@Configuration
+public class QuerydslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+    }
+}
